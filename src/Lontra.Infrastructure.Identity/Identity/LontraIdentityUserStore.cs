5377a06202c3deb6cad51940cd91d5dffd2cd8af
Goncalo Soares <goncalo.soares@byteconcept.pt>
2024-12-07 22:37:00 +0000

    Add LontraIdentityUserStore. Bump to 0.0.6

---
+++ src/Lontra.Infrastructure.Identity/Identity/LontraIdentityUserStore.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lontra.Identity;
+
+public class LontraIdentityUserStore : UserStore<LontraIdentityUser, IdentityRole<long>, LontraIdentityDbContext, long>
+{
+    public LontraIdentityUserStore(LontraIdentityDbContext context, IdentityErrorDescriber? describer = null) : base(context, describer) { }
+
+    public override async Task<LontraIdentityUser?> FindByIdAsync(string userId, CancellationToken cancellationToken = default)
+    {
+        var id = ConvertIdFromString(userId);
+        return await Context
+            .Users
+            .IgnoreQueryFilters()
+            .Where(u => u.Id == id)
+            .FirstOrDefaultAsync();
+    }
+
+    public override async Task<LontraIdentityUser?> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+    {
+        return await Context
+            .Users
+            .IgnoreQueryFilters()
+            .Where(u => u.NormalizedUserName == normalizedUserName)
+            .FirstOrDefaultAsync();
+    }
+}
