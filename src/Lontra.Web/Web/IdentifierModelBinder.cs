695bdb6b1bce2ac6ecf7e5b13bfc7084e4f7b1b7
Goncalo Soares <goncalo.soares@byteconcept.pt>
2024-12-31 00:04:14 +0000

    Add IdentifierModelBinders to Lontra.Web

---
+++ src/Lontra.Web/Web/IdentifierModelBinder.cs
@@ -0,0 +1,65 @@
+﻿using Lontra.Core;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Lontra.Web;
+
+public class IdentifierModelBinder : IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        if (bindingContext == null)
+        {
+            throw new ArgumentNullException(nameof(bindingContext));
+        }
+
+        var modelName = bindingContext.ModelName;
+
+        // Try to fetch the value of the argument by name
+        var valueProviderResult = bindingContext.ValueProvider.GetValue(modelName);
+
+        if (valueProviderResult == ValueProviderResult.None)
+        {
+            return Task.CompletedTask;
+        }
+
+        bindingContext.ModelState.SetModelValue(modelName, valueProviderResult);
+
+        // Read value as string
+
+        var value = valueProviderResult.FirstValue;
+
+        if (value == null)
+        {
+            return Task.CompletedTask;
+        }
+
+        if (typeof(Identifier<long>).IsAssignableFrom(bindingContext.ModelType) && long.TryParse(value, out var idLong))
+        {
+            var constructor = bindingContext.ModelType.GetConstructor([typeof(long)]);
+            var identifier = constructor!.Invoke([idLong]);
+
+            bindingContext.Result = ModelBindingResult.Success(identifier);
+            return Task.CompletedTask;
+        }
+
+        if (typeof(Identifier<int>).IsAssignableFrom(bindingContext.ModelType) && int.TryParse(value, out var idInt))
+        {
+            var constructor = bindingContext.ModelType.GetConstructor([typeof(int)]);
+            var identifier = constructor!.Invoke([idInt]);
+
+            bindingContext.Result = ModelBindingResult.Success(identifier);
+            return Task.CompletedTask;
+        }
+
+        if (typeof(Identifier<string>).IsAssignableFrom(bindingContext.ModelType))
+        {
+            var constructor = bindingContext.ModelType.GetConstructor([typeof(string)]);
+            var identifier = constructor!.Invoke([value]);
+
+            bindingContext.Result = ModelBindingResult.Success(identifier);
+            return Task.CompletedTask;
+        }
+
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Lontra.Web/Web/IdentifierModelBinderProvider.cs
@@ -0,0 +1,23 @@
+﻿using Lontra.Core.Reflection;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+namespace Lontra.Web;
+
+public class IdentifierBinderProvider : IModelBinderProvider
+{
+    public IModelBinder? GetBinder(ModelBinderProviderContext context)
+    {
+        if (context == null)
+        {
+            throw new ArgumentNullException(nameof(context));
+        }
+
+        if (typeof(IIdentifier).IsAssignableFrom(context.Metadata.ModelType))
+        {
+            return new BinderTypeModelBinder(typeof(IdentifierModelBinder));
+        }
+
+        return null;
+    }
+}
